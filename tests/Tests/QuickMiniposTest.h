2d43c91c7899ab794f916909fed93349f5cb28ee
Megaxela <xelamegaxela@gmail.com>
2017-03-06 18:57:26 +0300

    Добавил тест для гаснущего светодиода.

---
+++ tests/Tests/QuickMiniposTest.h
@@ -0,0 +1,20 @@
+//
+// Created by megaxela on 06.03.17.
+//
+
+#ifndef FRTESTER_QUICKMINIPOSTEST_H
+#define FRTESTER_QUICKMINIPOSTEST_H
+
+
+#include <include/Testing/AbstractTest.h>
+
+class QuickMiniposTest : public AbstractTest
+{
+public:
+    QuickMiniposTest();
+
+    bool execute() override;
+};
+
+
+#endif //FRTESTER_QUICKMINIPOSTEST_H
